d7225502ec038e8197f0454d21dd11d3cffe89a6
Srabanigorai <152902384+Srabanigorai@users.noreply.github.com>
2026-03-10 15:52:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-document.querySelectorAll('nav a').forEach(link => {
+// Smooth scrolling
+document.querySelectorAll('.navbar a').forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -8,7 +8,7 @@ document.querySelectorAll('nav a').forEach(link => {
   });
 });
 
-
+// Dark mode toggle
 const toggleButton = document.getElementById('darkModeToggle');
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
